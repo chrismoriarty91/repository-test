9ea7130d95e7941595fccda966ea3c45327f8e7d
Chris M <chrismoriarty91@gmail.com>
2019-05-01 22:50:32 +0000

    first commit

--- public/bundle.js
+++ public/bundle.js
@@ -705,19 +705,40 @@ function render(variables = {}) {
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
+  console.log(variables);
+  let name = "Sample";
+  if (variables.name !== null) name = variables.name;
+  let lastname = "McSample";
+  if (variables.lastname !== null) lastname = variables.lastname;
+  let city = "Miami";
+  if (variables.city !== null) city = variables.city;
+  let country = "USA";
+  if (variables.country !== null) country = variables.country;
+  let twitter = "NBA";
+  if (variables.twitter !== null) twitter = variables.twitter;
+  let github = "chrismoriarty91";
+  if (variables.github !== null) github = variables.github;
+  let linked = "paolo-castillo-20568796/";
+  if (variables.linkedin !== null) linked = variables.linkedin;
+  let instagram = "NBA";
+  if (variables.instagram !== null) instagram = variables.instagram;
+  let role = "Web Developer";
+  if (variables.role !== null) role = variables.role;
+  let SMP = "Unemployed";
+  if (variables.socialMediaPosition !== null) SMP = variables.socialMediaPosition;
 
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/alesanchezr"><i class="fa fa-twitter"></i></a></li>
-            <li><a href="https://github.com/alesanchezr"><i class="fa fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/alesanchezr"><i class="fa fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/alesanchezr"><i class="fa fa-instagram"></i></a></li>
+          <h1>${name} ${lastname}</h1>
+          <h2>${role}</h2>
+          <h3>${city}, ${country}</h3>
+          <ul class="position-${SMP}">
+            <li><a href="https://twitter.com/${twitter}"><i class="fa fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${github}"><i class="fa fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/in/${linked}"><i class="fa fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${instagram}"><i class="fa fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -738,7 +759,7 @@ window.onload = function () {
     socialMediaPosition: "left",
     // social media usernames
     twitter: null,
-    github: "alesanchezr",
+    github: null,
     linkedin: null,
     instagram: null,
     name: null,
